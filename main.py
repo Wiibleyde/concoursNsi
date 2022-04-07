5592713c54b472f6c4a71109597b93874ba44b84
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-04-07 21:14:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+""" TO DO
+
+- Recuperer donnees (web scrapping)
+(- Ajout des donnees dans base de donnees)
+- Analyser pour proposer des graphiques
+- Creer les graphiques (en js ou images mais pas ouf)
+- Interface utilisateur (flask)
+- 
+
+Nathan :
+
+Victor :
+
+
+"""
+
 import flask
