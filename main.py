d599e1024c910768cb9d686dde35e29df0e11c39
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-04-10 19:10:32 +0200

    repartition

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 """ TO DO
 
-- Recuperer donnees (web scrapping)
 (- Ajout des donnees dans base de donnees)
-- Analyser pour proposer des graphiques
-- Creer les graphiques (en js ou images mais pas ouf)
-- Interface utilisateur (flask)
-- 
 
 Nathan :
+- Recuperer donnees (web scrapping)
+- Analyser pour proposer des graphiques
+
 
 Victor :
-
+- Creer les graphiques (en js ou images mais pas ouf)
+- Interface utilisateur (flask)
 
 """
 
 import flask
 import random
 import sqlite3
+import csv
