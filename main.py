a3264b52ff778a629e0d8336ff41ba46d1c69266
RaptoUwU <victor-gardere@orange.fr>
2022-04-14 18:45:42 +0200

    html

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
-""" TO DO
-
-- Recuperer donnees (web scrapping)
+""" TODO
 (- Ajout des donnees dans base de donnees)
-- Analyser pour proposer des graphiques
-- Creer les graphiques (en js ou images mais pas ouf)
-- Interface utilisateur (flask)
-- 
-
 Nathan :
-
+- Recuperer donnees (web parsing)
+- Analyser pour proposer des graphiques
 Victor :
-
-
+- Creer les graphiques (en js ou images mais pas ouf(et ça c'est pas cool ;-;))
+- Interface utilisateur (flask)
+    - Possibilité de rentrer un csv à la main ou de récup csv d'une page
 """
 
 import flask
 import random
 import sqlite3
+import csv
+import pandas
+import requests
+from bs4 import BeautifulSoup
+
+
+def getCsvFile(url):
+    """get all csv file of a html page"""
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    csv_links = []
+    for link in soup.find_all('a'):
+        if link.get('href').endswith('.csv'):
+            csv_links.append(link.get('href'))
+    return csv_links
+
+def analyseCsv(csv_file):
+    """analyse a csv file"""
+    df = pandas.read_csv(csv_file)
+    return df
+
+# #On verra car on essaye de generer avec JavaScript
+# def generateChart(csv_file):
+#     """generate a chart from a csv file"""
+#     df = pandas.read_csv(csv_file)
+#     df.plot()
+#     return df
+
+
+
+def createIndexPage():
+    """create the main page"""
+    return flask.render_template('index.html')
