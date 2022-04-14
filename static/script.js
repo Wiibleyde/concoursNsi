5005871f54462b047731794615c47007c2e9b0ef
RaptoUwU <victor-gardere@orange.fr>
2022-04-14 22:09:42 +0200

    sélection de fichier, affichage upgraded

--- static/script.js
+++ static/script.js
@@ -1,1 +1,8 @@
 // js
+
+let inputFile = document.getElementById("select-file");
+let Filename = document.getElementById("file-name");
+inputFile.addEventListener("change", function(event){
+    let uploadFile = event.target.files[0].name;
+    Filename.textContent = uploadFile;
+})
