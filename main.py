5a7097003f243438db3538ce92f7074a09f56854
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-04-21 10:04:17 +0200

    update

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ Plan Flaskb (Victor) :
 """
 
 from cgitb import text
+from time import process_time_ns
+from turtle import down
 from flask import Flask, render_template, request
 import random
 import sqlite3
@@ -33,43 +35,42 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "MotDePasse"
 
-def getCsvFile(url):
-    """get all csv file of a html page"""
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    csv_links = []
-    for link in soup.find_all('a'):
-        if link.get('href').endswith('.csv'):
-            csv_links.append(link.get('href'))
-    return csv_links
+# def getCsvFile(url):
+#     """get all csv file of a html page"""
+#     page = requests.get(url)
+#     soup = BeautifulSoup(page.content, 'html.parser')
+#     csv_links = []
+#     for link in soup.find_all('a'):
+#         if link.get('href').endswith('.csv'):
+#             csv_links.append(link.get('href'))
+#     return csv_links
 
-def analyseCsv(csv_file):
-    """analyse a csv file"""
-    df = pandas.read_csv(csv_file)
-    return df
+# def analyseCsv(csv_file):
+#     """analyse a csv file"""
+#     df = pandas.read_csv(csv_file)
+#     return df
 
-def getLink(url):
-    """get all csv file of a html page"""
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    dlLink = []
-    for link in soup.find_all('a'):
-        dlLink.append(link.get('href'))
-    return dlLink
+# def getLink(url):
+#     """get all csv file of a html page"""
+#     page = requests.get(url)
+#     soup = BeautifulSoup(page.text, 'html.parser')
+#     dlLink = []
+#     for link in soup.find_all('a'):
+#         dlLink.append(link.get('href'))
+#     return dlLink
 
-def sortList(lst):
-    newLst = []
-    for compteur in lst:
-        if compteur == None:
-            pass
-        else:
-            urlStr=[]
-            print(compteur)
-            urlStr=compteur.split('/')
-            for word in urlStr:
-                if 'https://www.data.gouv.fr/fr/datasets/' in word or 'download' in word or 'dataset' in word:
-                    newLst.append(compteur)
-    return newLst
+# def sortList(lst):
+#     newLst = []
+#     for compteur in lst:
+#         if compteur == None:
+#             pass
+#         else:
+#             urlStr=[]
+#             urlStr=compteur.split('/')
+#             for word in urlStr:
+#                 if 'https://www.data.gouv.fr/fr/datasets/' in word or 'download' in word or 'dataset' in word:
+#                     newLst.append(compteur)
+    # return newLst
 
 def download(lst): 
     """download all file of lst"""     
@@ -81,11 +82,7 @@ def download(lst):
             name = f'file{num}'
             r = requests.get(url, allow_redirects=True)
             open(f'temp\\{name}', 'wb').write(r.content)
-            if isJson(name):
-                os.rename(f'temp\\{name}', f'files\\{name}.json')
-                print(f'{name} is json saving it')
-                num=num+1
-            elif isCsv(name):
+            if isCsv(name):
                 os.rename(f'temp\\{name}', f'files\\{name}.csv')
                 print(f'{name} is csv saving it')
                 num=num+1
@@ -107,17 +104,17 @@ def isCsv(file):
     except UnicodeDecodeError:
         return False
 
-def isJson(file):
-    """check if a file is a json file by reading its first line"""
-    try:
-        with open(f'temp\\{file}', 'r') as f:
-            first_line = f.readline()
-            if '{' in first_line:
-                return True
-            else:
-                return False
-    except UnicodeDecodeError:
-        return False
+# def isJson(file):
+#     """check if a file is a json file by reading its first line"""
+#     try:
+#         with open(f'temp\\{file}', 'r') as f:
+#             first_line = f.readline()
+#             if '{' in first_line:
+#                 return True
+#             else:
+#                 return False
+#     except UnicodeDecodeError:
+#         return False
           
 
 @app.route("/", methods=["GET", "POST"])
@@ -134,8 +131,8 @@ def Show_Graph():
     if lien == "":
         print('empty')
     else:
-        print(lien)
+        download(lien)
     return render_template("Show_Graph.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
