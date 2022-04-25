7651fa9c3f77f1910a0694042eb589c2b11f5ec5
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-04-25 18:41:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,10 +25,12 @@ from flask import Flask, render_template, request
 import random
 import sqlite3
 import csv
-import pandas
 import requests
 from bs4 import BeautifulSoup
 import os
+import pandas as pd
+import openpyxl
+
 
 
 app = Flask(__name__)
@@ -71,6 +73,102 @@ app.config["SECRET_KEY"] = "MotDePasse"
 #                 if 'https://www.data.gouv.fr/fr/datasets/' in word or 'download' in word or 'dataset' in word:
 #                     newLst.append(compteur)
     # return newLst
+
+
+class File:
+    def __init__(self, fileName):
+        self.fileName = fileName
+
+    def isCsv(self):
+        """check if a file is a csv file by reading its first line"""
+        try:
+            with open(self.fileName, 'r') as f:
+                first_line = f.readline()
+                if ',' in first_line or ';' in first_line:
+                    return True
+                else:
+                    return False
+        except UnicodeDecodeError:
+            return False
+
+    def isXlsx(self):
+        try:
+            excel=openpyxl.load_workbook(self.fileName)
+            return True
+        except openpyxl.utils.exceptions.InvalidFileException:
+            return False
+
+
+    def getFileName(self):
+        return self.fileName.split('\\')[-1].split('.')[0]
+
+    def fieldNames(self):
+        """returns the field names of a csv or xlsx file"""
+        if self.isCsv():
+            with open(self.fileName, 'r') as f:
+                reader = csv.reader(f, delimiter=';')
+                firstLine=next(reader)
+                cols=firstLine #[0].split(';')
+                for compteur in range(len(cols)):
+                    cols[compteur]="'"+cols[compteur].replace(' ','_')+"'"
+                return cols               
+        elif self.isXlsx():
+            excel = openpyxl.load_workbook(self.fileName)
+            sheet = excel.active
+            cols=[]
+            firstRow=next(sheet.rows)
+            for c in firstRow:
+                cols.append(c.value)
+            print(cols)
+            for compteur in range(len(cols)):
+                if cols[compteur] is not None:
+                    cols[compteur]="'"+cols[compteur].replace(' ','_')+"'"
+                else:
+                    cols[compteur]="'"+str(compteur)+"'"
+            return cols   
+        else:
+            print("This file is not a csv file")
+
+    def copyToSQLite(self,dbName):
+        """read a csv file and write it to a sqlite database"""
+        if self.isCsv():
+            with open(self.fileName, 'r') as f:
+                reader = csv.reader(f, delimiter=';')
+                con = sqlite3.connect(dbName)
+                cur = con.cursor()
+                con.set_trace_callback(print)
+                cur.execute("DROP TABLE IF EXISTS '{}'".format(self.getFileName()))
+                cur.execute("CREATE TABLE '{}' ({})".format(self.getFileName(),','.join(self.fieldNames())))
+                next(reader)
+                for row in reader:
+                    sql="INSERT INTO '{}' VALUES (?{})".format(self.getFileName(),(len(self.fieldNames())-1)*",?")
+                    cur.execute(sql,row)
+                con.commit()
+        elif self.isXlsx():
+            excel = openpyxl.load_workbook(self.fileName)
+            sheet = excel.active
+            con = sqlite3.connect(dbName)
+            cur = con.cursor()
+            con.set_trace_callback(print)
+            cur.execute("DROP TABLE IF EXISTS '{}'".format(self.getFileName()))
+            cur.execute("CREATE TABLE '{}' ({})".format(self.getFileName(),','.join(self.fieldNames())))
+            for r in sheet.rows:
+                sql="INSERT INTO '{}' VALUES (?{})".format(self.getFileName(),(len(self.fieldNames())-1)*",?")
+                cur.execute(sql,[cell.value for cell in r])
+            con.commit()
+        else:
+            print("This file is not a csv file")
+
+    def convertToCSV(self):
+        excel = openpyxl.load_workbook(self.fileName)
+        sheet = excel.active
+        name=self.getFileName()
+        col = csv.writer(open(f'files\\{name}.csv','w',newline="",encoding="utf-8"))
+        for r in sheet.rows:
+            col.writerow([cell.value for cell in r])
+        df = pd.DataFrame(pd.read_csv(f'files\\{name}.csv'))
+        return df
+
 
 def download(lst): 
     """download all file of lst"""     
