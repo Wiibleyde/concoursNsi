f4b74d30fcd70cd1c2a29badb196420796eef91f
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-04-26 13:41:27 +0200

    comiit

--- main.py
+++ main.py
@@ -1,33 +1,10 @@
-""" TODO
-(- Ajout des donnees dans base de donnees)
-Nathan :
-- Recuperer donnees (web parsing) (et les mettre dans une base de donnees)
-- Analyser pour proposer des graphiques 
-
-
-Victor :
-(En cours) - Afficher des graph (en js ou images mais pas ouf(et ça c'est pas cool ;-;)) 
-(Terminé) - Interface utilisateur (flask) 
-    (Terminé) - Possibilité de rentrer un csv à la main ou de récup csv d'une page
-
-Plan Flaskb (Victor) :
-(Terminé) - Creer une page d'accueil 
-(Terminé) - Creer un page pour rentrer des fichier csv à partir d'une page web ou d'un fichier mis à la main 
-(Terminé) - Creer une page qui propose les graphiques à partir du fichier csv avec un bouton pour revenir a l'acceuil 
-
-
-"""
-
 from flask import Flask, render_template, request, g
 import sqlite3
 import csv
 import requests
-from bs4 import BeautifulSoup
 import os
 import pandas as pd
 import openpyxl
-
-
 
 app = Flask(__name__)
 
@@ -36,10 +13,19 @@ app.config["SECRET_KEY"] = "MotDePasse"
 class File:
     def __init__(self, fileName):
         self.fileName = fileName
-
+        if 'http' in fileName:
+            self.download(fileName)
+            if self.isCsv(): 
+                self.fileName = f'files\\{self.getFileName()}.csv'
+            elif self.isXlsx():
+                self.fileName = f'files\\{self.getFileName()}.xlsx'
+            else:
+                print("This file is not a csv file")
+                
     def isCsv(self):
         """check if a file is a csv file by reading its first line"""
         try:
+            print(self.fileName)
             with open(self.fileName, 'r') as f:
                 first_line = f.readline()
                 if ',' in first_line or ';' in first_line:
@@ -56,14 +42,34 @@ class File:
         except openpyxl.utils.exceptions.InvalidFileException:
             return False
 
+    def download(self,url): 
+        """download all file of lst"""     
+        try:
+            print(f'Downloading {url}')
+            name = self.getFileName()
+            r = requests.get(url, allow_redirects=True)
+            open(f'temp\\{name}', 'wb').write(r.content)
+            if isCsv(name):
+                os.rename(f'temp\\{name}', f'files\\{name}.csv')
+                print(f'{name} is csv saving it')
+            else:
+                os.remove(f'temp\\{name}')
+                print(f'{name} is not csv or json deleting it')
+        except requests.exceptions.MissingSchema:
+            print(f'{url} is not a valid url')
+
+        return f'files\\{self.getFileName()}.xlsx'
 
     def getFileName(self):
-        return self.fileName.split('\\')[-1].split('.')[0]
+        if 'http' in self.fileName:
+            return self.fileName.split('/')[-1].split('.')[0]
+        else:
+            return self.fileName.split('\\')[-1].split('.')[0]
 
     def fieldNames(self):
         """returns the field names of a csv or xlsx file"""
         if self.isCsv():
-            with open(self.fileName, 'r') as f:
+            with open(self.getfileName(), 'r') as f:
                 reader = csv.reader(f, delimiter=';')
                 firstLine=next(reader)
                 cols=firstLine #[0].split(';')
@@ -71,7 +77,7 @@ class File:
                     cols[compteur]="'"+cols[compteur].replace(' ','_')+"'"
                 return cols               
         elif self.isXlsx():
-            excel = openpyxl.load_workbook(self.fileName)
+            excel = openpyxl.load_workbook(self.getfileName())
             sheet = excel.active
             cols=[]
             firstRow=next(sheet.rows)
@@ -136,27 +142,6 @@ class File:
         con.commit()
         return lstTitle
 
-
-def download(lst): 
-    """download all file of lst"""     
-    num=0
-    name=f'file{num}'
-    for url in lst:
-        try:
-            print(f'Downloading {url}')
-            name = f'file{num}'
-            r = requests.get(url, allow_redirects=True)
-            open(f'temp\\{name}', 'wb').write(r.content)
-            if isCsv(name):
-                os.rename(f'temp\\{name}', f'files\\{name}.csv')
-                print(f'{name} is csv saving it')
-                num=num+1
-            else:
-                os.remove(f'temp\\{name}')
-                print(f'{name} is not csv or json deleting it')
-        except requests.exceptions.MissingSchema:
-            print(f'{url} is not a valid url')
-
 def isCsv(file):
     """check if a file is a csv file by reading its first line"""
     try:
@@ -197,9 +182,7 @@ def Show_Graph():
     return render_template("Show_Graph.html", data=data)
 
 if __name__ == "__main__":
-    fichier1=File("files\\46TP-TA_Presidentielle_2022__1er au 16 janvier 2022_v5.csv")
-    #lstFichier1=fichier1.copyToSQLite("files\\Database.db")
+    fichier1=File("https://www.observatoires-des-loyers.org/datagouv/2021/Base_OP_2021_Nationale.csv")
+    lstFichier1=fichier1.copyToSQLite("files\\Database.db")
     machin = fichier1.getTitle("files\\Database.db")
     app.run()
-
-
