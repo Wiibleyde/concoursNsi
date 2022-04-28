fa29c14b6516dcb99b59a9fd286fabf788ba5c82
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-04-28 20:12:00 +0200

    update form 2

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 // js
 
 let inputFile = document.getElementById("select-file");
-let Filename = document.getElementById("file-name");
+let Filename = document.getElementById("file-name-display");
 inputFile.addEventListener("change", function(event){
     let uploadFile = event.target.files[0].name;
     Filename.textContent = uploadFile;
